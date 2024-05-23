216eea2f5057df233ae714f2d79b19ea0bfef7d2
nsimonin1 <nsimonin1@gmail.com>
2024-05-23 08:09:10 +0200

    Init project

---
+++ app-poc/src/main/java/org/afrinnov/apppoc/AppPocApplication.java
@@ -0,0 +1,13 @@
+package org.afrinnov.apppoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPocApplication.class, args);
+    }
+
+}
